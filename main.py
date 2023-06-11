15c0952d12d64099758b7861f34b1b0aadb2dc2f
NjokiFlo <102618427+NjokiFlo@users.noreply.github.com>
2023-06-11 23:48:16 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+word1 = input("Enter the word to replace: ")
+word2 = input("Enter the word to replace with: ")
+
+print("Your name is  " + name)
+print(word1 + word2)
